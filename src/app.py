76f154a19af193e4b1843d6ed434f04d91154b41
Gabriel <github@delitefully.com>
2021-12-25 01:43:31 +0200

    fix: error message key name consistency

--- src/app.py
+++ src/app.py
@@ -30,7 +30,7 @@ def get_track_canvas(track_id):
         canvas_url = canvas.get_canvas_for_track(access_token, track_id)
         return {'success': 'true', 'canvas_url': canvas_url}
     except AttributeError:
-        return {'success': 'false', 'error': 'No canvas found for this track'}
+        return {'success': 'false', 'message': 'No canvas found for this track'}
     except ConnectionError:
         return {'success': 'false', 'message': 'failed to connect to Spotify'}
 
